4c449ce8899e2207490a145394bc300a7228f4b5
Dawid Neumann <dawid.neumann@gmail.com>
2026-07-04 12:15:01 +0200

    Initial commit

---
+++ src/main/java/com/dneumann/raben1/Raben1Application.java
@@ -0,0 +1,13 @@
+package com.dneumann.raben1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Raben1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Raben1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dneumann/raben1/Raben1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dneumann.raben1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Raben1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
